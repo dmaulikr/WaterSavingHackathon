46cc74b848540ff937224d000ff12f599cbfef02
Sam Showalter <samwam2@gmail.com>
2016-01-30 17:36:03 -0800

    Parse working
    Parse is working even through its dying in a year.. RIP Parse…

--- WaterSavingHackathon/AppDelegate.swift
+++ WaterSavingHackathon/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.setApplicationId("sqlSc0QDAPyXs5E2RFZJ0yzEP7SMY1zvIwe4mzDY", clientKey: "h5xyR0BaOARugLwrj6cbhgUZmGSd4abNnAuwRP3H")
+        PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
         return true
     }
 
--- WaterSavingHackathon/ViewController.swift
+++ WaterSavingHackathon/ViewController.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
+import Parse
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let user = PFUser()
+        user.username = "my name"
+        user.password = "my pass"
+        user.email = "lololol@example.com"
+        
+        user.signUpInBackgroundWithBlock {
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error {
+                let errorString = error.userInfo["error"] as? NSString
+                // Show the errorString somewhere and let the user try again.
+            } else {
+                // Hooray! Let them use the app now.
+            }
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
